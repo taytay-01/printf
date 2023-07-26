098982ad9f3ba542cb2dfd3692c5723446e305af
taytay01 <taytayfatimazahra@gmail.com>
2023-07-25 18:00:57 -0700

    main

--- _printf.c
+++ _printf.c
@@ -1,11 +1,11 @@
 #include "main.h"
-void print_buffer(char buffer[],int *buff_ind);
+void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
  */
-int _printf(const char *format,...)
+int _printf(const char *format, ...)
 {
 
 int i, printed = 0, printed_chars = 0;
